d448176554856033b97b82b2c15d596c9c07dbbc
abdias <abadias618@gmail.com>
2019-12-02 03:15:36 -0700

    12/2/2019 3.15am

--- index.js
+++ index.js
@@ -2,16 +2,17 @@ const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
 const bodyParser = require('body-parser')
-express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+const app = express()
+
+  app.use(express.static(path.join(__dirname, 'public')))
+  app.set('views', path.join(__dirname, 'views'))
+  app.set('view engine', 'ejs')
+  app.get('/', (req, res) => res.render('pages/index'))
+  app.listen(PORT, () => console.log(`Listening on ${ PORT }`))
   //for body parser
-  .use(bodyParser.urlencoded({extended: false}))
+  app.use(bodyParser.urlencoded({extended: false}))
   //POST method
-  .post('/calculate_price',(req,res)=>{
+  app.post('/calculate_price',(req,res)=>{
     var weight = req.body.weight;
     console.log('the weight you entered is '+weight);
     res.end();
